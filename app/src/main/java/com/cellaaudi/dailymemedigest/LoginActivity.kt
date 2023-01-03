20bbbf8958de8479fb9a7453e96505504a5167d5
cellaaudi <cellaaudi27@gmail.com>
2023-01-04 02:29:50 +0700

    basic ui

---
+++ app/src/main/java/com/cellaaudi/dailymemedigest/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.cellaaudi.dailymemedigest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        btnCraeteAcc.setOnClickListener{
+
+        }
+    }
+}
