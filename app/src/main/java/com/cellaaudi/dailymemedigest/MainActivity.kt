487c7da254bd6790d101b9900b42ea0de494e0e2
cellaaudi <cellaaudi27@gmail.com>
2023-01-04 00:58:50 +0700

    navbar

--- app/src/main/java/com/cellaaudi/dailymemedigest/MainActivity.kt
+++ app/src/main/java/com/cellaaudi/dailymemedigest/MainActivity.kt
@@ -2,10 +2,42 @@ package com.cellaaudi.dailymemedigest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val fragments: ArrayList<Fragment> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fragments.add(HomeFragment())
+        fragments.add(MineFragment())
+        fragments.add(LeaderboardFragment())
+        fragments.add(SettingsFragment())
+
+        viewPager.adapter = MyAdapter(this, fragments)
+
+        viewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                bottomNav.selectedItemId = bottomNav.menu.getItem(position).itemId
+            }
+        })
+
+        bottomNav.setOnItemSelectedListener {
+            viewPager.currentItem = when(it.itemId) {
+                R.id.itemHome -> 0
+                R.id.itemMine -> 1
+                R.id.itemLeaderboard -> 2
+                R.id.itemSettings -> 3
+
+                else -> 0
+            }
+
+            true
+        }
     }
 }
